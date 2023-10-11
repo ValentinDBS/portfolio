8f82070b5dddbe4ffd3ce2fc0f992875dab65c7f
ValentinDBS <pandarcha@outlook.fr>
2023-10-11 12:11:32 +0200

    update

--- app.js
+++ app.js
@@ -24,3 +24,15 @@ allBoxes.forEach(box => {
     }
 
 })
+
+// Nav
+
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
